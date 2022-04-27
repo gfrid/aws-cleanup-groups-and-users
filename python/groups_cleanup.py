e6ce26e7a14663d9dd60f1b17bd28925788ccc5e
George <97164735+gfrid@users.noreply.github.com>
2022-04-27 14:29:27 +0300

    updated_exception_handle

--- python/groups_cleanup.py
+++ python/groups_cleanup.py
@@ -1,17 +1,18 @@
 import boto3
+import botocore
 session = boto3.session.Session(profile_name='default')
 
 import json
 
-f = open('C:\\temp\managed_list_of_accounts.json')
+f = open('C:\\managed.json')
 data = json.load(f)
 for i in data['accounts']:
     print(i)
     try:
         sts_client = boto3.client('sts')
         assumed_role_object=sts_client.assume_role(
-            RoleArn="arn:aws:iam::" + i + ":role/Automation",
-            RoleSessionName="AssumeRoleSessionAutomation"
+            RoleArn="arn:aws:iam::" + i + ":role/IT_Automation",
+            RoleSessionName="AssumeRoleSession"
         )
         print(assumed_role_object)
         credentials=assumed_role_object['Credentials']
@@ -22,69 +23,106 @@ for i in data['accounts']:
             aws_secret_access_key=credentials['SecretAccessKey'],
             aws_session_token=credentials['SessionToken'],
         )
-    except:
-        break
+    except Exception as eee:
+        print(eee)
+        continue
+
+    #iam = boto3.client('iam')
 
     print("starting cleanup in account: " + i)
     response = iam.list_groups()
     for group in response['Groups']:
         group_details = iam.get_group(GroupName=group['GroupName'])
         for user in group_details['Users']:
-            if "dev" in group['GroupName']:
-                dev_users = (user['UserName'])
-                dev_group = (group['GroupName'])
-                print(dev_users)
-                print(dev_group)
-                for users in dev_group:
+            #print(" - ", user['UserName'])
+            if "test" in group['GroupName']:
+                test_users = (user['UserName'])
+                test_group = (group['GroupName'])
+                print(test_users)
+                print(test_group)
+                for users in test_group:
                     response_remove_users = iam.remove_user_from_group(
-                        GroupName=dev_group,
-                        UserName=dev_users,
+                        GroupName=test_group,
+                        UserName=test_users,
                     )
                 print(response_remove_users)
 
 
-        if "dev" in group['GroupName']:
-            dev_group = (group['GroupName'])
+        if "test" in group['GroupName']:
+            test_group = (group['GroupName'])
             global policy_names
             global arn
-            for group in dev_group:
-                print(dev_group)
-                attached_group_policies = (iam.list_attached_group_policies(GroupName=dev_group)['AttachedPolicies'])
+            for group in test_group:
+                print(test_group)
+                attached_group_policies = (iam.list_attached_group_policies(GroupName=test_group)['AttachedPolicies'])
                 print(attached_group_policies)
                 for policy in attached_group_policies:
                     arn = policy['PolicyArn']
-                    response_remove_polices = iam.detach_group_policy(
-                            GroupName=dev_group,
-                            PolicyArn=arn,
-                    )
+                    try:
+                        response_remove_polices = iam.detach_group_policy(
+                                GroupName=test_group,
+                                PolicyArn=arn,
+                        )
+                    except Exception as eee:
+                        print(eee)
+                        continue
                     print(response_remove_polices)
-            response_delete_group = iam.delete_group(
-                GroupName=dev_group
-            )
-            print(response_delete_group)
+            try:
+                response_delete_group = iam.delete_group(
+                    GroupName=test_group
+                )
+                print(response_delete_group)
+            except Exception as eee:
+                print(eee)
+                continue
 
 
-    response_dev_iam_users = iam.list_users()
-    for iam_user in response_dev_iam_users['Users']:
-        if "dev" in iam_user['UserName']:
-            dev_user = iam_user['UserName']
-            print(dev_user)
+    response_test_iam_users = iam.list_users()
+    for iam_user in response_test_iam_users['Users']:
+        if "test" in iam_user['UserName']:
+            test_user = iam_user['UserName']
+            print(test_user)
             global policy_names
             global iam_arn
-            attached_user_policies = (iam.list_attached_user_policies(UserName=dev_user)['AttachedPolicies'])
+            access_key_id = None
+            list_access_keys_to_remove = []
+            attached_user_policies = (iam.list_attached_user_policies(UserName=test_user)['AttachedPolicies'])
             print(attached_user_policies)
             for policy in attached_user_policies:
                 iam_arn = policy['PolicyArn']
                 response_remove_user_polices = iam.detach_user_policy(
-                    UserName=dev_user,
+                    UserName=test_user,
                     PolicyArn=iam_arn,
                 )
                 print(response_remove_user_polices)
+
+            try:
+                res_keys = iam.list_access_keys(
+                    UserName=test_user,
+                    MaxItems=4)
+                for key in res_keys['AccessKeyMetadata']:
+                     if 'AccessKeyId' in key:
+                         access_key_id = key['AccessKeyId']
+                         list_access_keys_to_remove.append(access_key_id)
+            except Exception as eee:
+                print(eee)
+                print("An error occurred while listing access keys")
+            try:
+                for access_key_id in list_access_keys_to_remove:
+                    print('Access key {0} needs to be removed'.format(access_key_id))
+                    response_delete_keys = iam.delete_access_key(
+                        UserName=test_user,
+                        AccessKeyId=access_key_id
+                    )
+                    print(response_delete_keys)
+            except Exception as eee:
+                print(eee)
+                print("something_went_wrong " + test_user)
             try:
                 response_delete_iam_user = iam.delete_user(
-                    UserName=dev_user
+                    UserName=test_user
                 )
                 print(response_delete_iam_user)
-            except:
-                print("user has keys, ignore: " + dev_user)
-                pass
+            except Exception as eee:
+                print(eee)
+                continue
